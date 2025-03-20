bbcab8a6ea0419d51e083ed51c73ab13f1e33fcf
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-03-20 15:46:41 +0900

    feat:example

--- examples/create_token.rs
+++ examples/create_token.rs
@@ -1,8 +1,15 @@
+use dotenv::dotenv;
 use korea_investment_rs::oauth::Oauth;
+use std::env;
+use std::slice::GetDisjointMutError;
 #[tokio::main]
 async fn main() {
-    let app_key = "".to_string();
-    let app_secret = "".to_string();
+    dotenv().ok();
+
+    let app_key = env::var("PUB_KEY").expect("APP_KEY not set in .env file");
+    let app_secret = env::var("SCREST_KEY").expect("APP_SECRET not set in .env file");
+
     let oauth = Oauth::new(app_key, app_secret);
     let token = Oauth::get_access_token(&oauth).await.unwrap();
+    println!("{}", token);
 }
